2c2aa71a20e97adf32812fe4658807a7531cf0e7
elifbngskc <istekuzukuzugel@gmail.com>
2024-12-02 15:06:02 +0300

    "fixed migration error"

--- db/migrate/20241202105048_add_user_to_posts.rb
+++ db/migrate/20241202105048_add_user_to_posts.rb
@@ -1,8 +1,13 @@
 class AddUserToPosts < ActiveRecord::Migration[7.2]
   def change
+    unless column_exists?(:posts, :user_id)
+      add_column :posts, :user_id, :integer, null: false
+    end
+
     unless foreign_key_exists?(:posts, :users)
       add_foreign_key :posts, :users
     end
+
     unless index_exists?(:posts, :user_id)
       add_index :posts, :user_id
     end
