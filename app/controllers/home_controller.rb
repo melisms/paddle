54e4732c313af8cb033f6b811e54ab53569bebd6
Melisa <melisa.muslu@live.acibadem.edu.tr>
2024-12-19 11:38:52 +0300

    main_page show post

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
     @username = current_user.username
+    @posts = Post.all.order(created_at: :desc)
   end
 end
