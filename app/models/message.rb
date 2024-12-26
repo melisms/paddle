19a85162fa5a79ef20ec4a996f6c821a49695371
melisms <117736099+melisms@users.noreply.github.com>
2024-12-26 15:06:35 +0300

    add img -> msg

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,9 @@ class Message < ApplicationRecord
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
 
-  validates :content, presence: true
+  has_one_attached :image
+
+  validates :content, presence: true, unless: -> { image.attached? }
   validates :sender_id, presence: true
   validates :receiver_id, presence: true
 
