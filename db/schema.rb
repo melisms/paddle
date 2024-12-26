3bb98ec11e21d20d8bc7ba5338834983bb7cd7fe
elifbngskc <istekuzukuzugel@gmail.com>
2024-12-26 21:20:22 +0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -96,6 +96,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_26_180252) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "age"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
@@ -134,8 +135,8 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_26_180252) do
     t.integer "role", default: 0, null: false
     t.string "username"
     t.text "profile_description"
-    t.integer "followers_count"
-    t.integer "following_count"
+    t.integer "followers_count", default: 0
+    t.integer "following_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
