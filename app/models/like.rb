570bfabf3bc461df198c7a81e499778682abd3d8
elifbngskc <istekuzukuzugel@gmail.com>
2024-12-29 22:41:03 +0300

    like bug fix(notifs are still not working)

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,14 +3,12 @@ class Like < ApplicationRecord
   belongs_to :post
   validates :user_id, uniqueness: { scope: :post_id }
 
-  after_create :notify_post_owner
+  #after_create :notify_post_owner
 
   private
 
-  def notify_post_owner
-    # Avoid notifying the post's owner if they liked their own post
-    return if post.user == user
-
-    LikeNotification.with(like: self).deliver_later(post.user)
-  end
+  #def notify_post_owner
+    #return if post.user == user
+    #LikeNotification.with(like: self).deliver_later(post.user)
+  #end
 end
