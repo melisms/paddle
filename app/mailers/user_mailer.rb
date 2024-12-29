b5a578e33602b8b5b95ca6d917c7594c8db97b5b
Melisa <melisa.muslu@live.acibadem.edu.tr>
2024-12-29 17:07:32 +0300

    add mail feature

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,8 @@ class UserMailer < ApplicationMailer
   include Rails.application.routes.url_helpers
   def welcome_email(user)
     @user = user
-    # @url  = "https://evident-massive-camel.ngrok-free.app/login"  # Update with your production URL
+    # @url  = "https://evident-massive-camel.ngrok-free.app/login"
+    @url = "http://127.0.0.1:3000/users/sign_in"
     mail(to: @user.email, subject: "Welcome to Our App!")
   end
 end
