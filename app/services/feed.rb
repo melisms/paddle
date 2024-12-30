ee67d182e843be421cb899717005b457cac11606
elifbngskc <istekuzukuzugel@gmail.com>
2024-12-30 23:29:32 +0300

    i dont know why i do this part by part

--- app/services/feed.rb
+++ app/services/feed.rb
@@ -7,17 +7,11 @@ class Feed
   def fetch_feed
     cache_key = "user_feed_#{@user.id}"
 
-
     Rails.cache.fetch(cache_key, expires_in: 10.minutes) do
-
       followed_user_ids = @user.followed_users.pluck(:id) << @user.id
-
       followed_posts = Post.where(user_id: followed_user_ids).order(created_at: :desc)
-
       random_posts = Post.where.not(user_id: @user.id).order("RANDOM()").limit(5)
-
       combined_posts = (followed_posts + random_posts).sort_by(&:created_at).reverse
-
       combined_posts
     end
   end
