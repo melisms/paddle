f2ecfa1700ec981d984dc58986e6712502364dfd
elifbngskc <istekuzukuzugel@gmail.com>
2024-12-30 14:37:35 +0300

    sign up fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
+  allow_browser versions: :modern
   before_action :set_query, :set_user
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   def set_query
     @query = Post.ransack(params[:q])
   end
