0e247364c825a3e140307d739aeb204282a9ea56
elifbngskc <istekuzukuzugel@gmail.com>
2024-12-30 12:01:02 +0300

    UPGRADEEEE

--- app/notifiers/like_notifier.rb
+++ app/notifiers/like_notifier.rb
@@ -1,12 +1,8 @@
-
 class LikeNotifier < ApplicationNotifier
+  deliver_by :database, target: -> { params[:like].post.user }
 
-  deliver_by :database, target: ->(like) { like.post.user } # Send notification to the post's user
-
-  # Define the notification content
-
-  def message(recipient)
-    "#{@like.user.username} liked your post"
+  def message(_recipient)
+    "#{params[:username]} liked your post"
   end
-
 end
+
